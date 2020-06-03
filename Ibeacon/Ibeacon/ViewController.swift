eb004335d3fcfd4de31a6e692d285aa087858917
ql9 <49939582+ql9@users.noreply.github.com>
2020-06-03 20:10:23 +0700

    Add files via upload

---
+++ Ibeacon/Ibeacon/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Ibeacon
+//
+//  Created by Cntt35 on 6/3/20.
+//  Copyright © 2020 hcmute. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    
+    var locationManager:CLLocationManager=CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "62b96558-b588-499d-8987-068bf10f3124")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 0, minor: 0, identifier: "MyBeacon")
+        
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            updateDistance(beacons[0].proximity)
+        } else {
+            updateDistance(.unknown)
+        }
+    }
+    
+    func updateDistance(_ distance: CLProximity) {
+        UIView.animate(withDuration: 0.8) {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+            }
+        }
+    }
+
+}
+
